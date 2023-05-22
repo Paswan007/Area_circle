140da75dc6d32c3aff4f132e71a7eafe15488783
Paswan007 <118629946+Paswan007@users.noreply.github.com>
2023-05-22 08:27:14 +0530

    Add files via upload

---
+++ c++2 - Copy - Copy.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float r,area,circum;
+    cout<<"Enter the radius of circle";
+    cin>>r;
+    cout<<"Area of circle is :"<<3.14*r*r<<endl;
+    cout<<"Circumference of circle is :"<<2*3.14*r;
+	return 0;
+}
